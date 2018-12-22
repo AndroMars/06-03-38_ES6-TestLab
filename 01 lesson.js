bbd262f6c05ca6a39451cc21345225ff45b64cf6
Coeryth <marcel2.koch@tu-dortmund.de>
2018-12-22 13:58:17 +0100

    0.2

---
+++ 01 lesson.js
@@ -0,0 +1,15 @@
+
+// LET AND CONST
+
+
+const point2 = {
+	x: 10,
+  	y: 25,
+  	z: -34,
+  	a: 88,
+  	type: 'doner',
+  	color: 'heavyblack',
+  	carat: 'fullmetal'
+}
+
+console.log(point2.x, point2.y, point2.z, point2.a, point2.type, point2.color, point2.carat);
