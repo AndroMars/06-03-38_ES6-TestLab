871d104940adb459343351652471734d40828ac1
Coeryth <marcel2.koch@tu-dortmund.de>
2018-12-22 15:12:45 +0100

    0.2

--- 01 lesson.js
+++ 01 lesson.js
@@ -1,5 +1,7 @@
 
-// LET AND CONST
+// Bale ES6
+
+//LET A = variabel ND CONST = konstante
 
 
 const point2 = {
@@ -13,3 +15,38 @@ const point2 = {
 }
 
 console.log(point2.x, point2.y, point2.z, point2.a, point2.type, point2.color, point2.carat);
+
+// 0.1
+
+
+const point3 = {
+	x2: 10,
+  	y2: 25,
+  	type2: 'doner',
+  	color2: 'heavyblack123'
+}
+
+const hallo = ({x2, y2, type2, color2}) => {
+	console.log(x2, y2, type2, color2);
+}
+
+hallo(point3);
+
+// 0.2 Destructuring - Nehmen wir den Kram auseiander ?!???????
+
+
+const circle = {
+    radius: 10,
+    getArea: (redius) => {
+      return Math.PI * radius * radius;
+    },
+  };
+  
+  const { radius, getArea } = circle;
+  getArea(radius);
+
+
+// Typescript
+
+
+
