f18c824cac539c92a1ebf9e1d71e7977a5bb0394
fr <rg>
2019-04-13 22:17:23 +0200

    es6.2

--- CC/ES6.js
+++ CC/ES6.js
@@ -1,10 +1,25 @@
 var catName;
 var quote;
-function catTalk() {
-  "use strict";
 
-  catName = "Oliver";
-  quote = catName + " says Meow!";
+function catTalk() {
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
+
+}
+catTalk();
+
+//  transform into Let
+
+let catName;
+let quote;
+
+function catTalk() {
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
 
 }
 catTalk();
